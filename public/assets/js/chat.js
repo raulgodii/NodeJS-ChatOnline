1b9b0b1deb88054afa680d0ed30de4ce2d901fc4
raulgodii <raulgodii13@gmail.com>
2024-02-24 22:12:42 +0100

    receive msg

--- public/assets/js/chat.js
+++ public/assets/js/chat.js
@@ -1,11 +1,57 @@
-let chat;
+let chat = document.getElementById("chat");
+const socket = io();
 
-window.onload = () => {
+socket.on('sendMessage', (response) => {
+    response = JSON.parse(response);
+    console.log('Recibo ' + response.msg)
 
-    chat = document.getElementById("chat");
-
-
-}
+    const chatMessages = document.getElementById("chat-messages");
+    const messageContent = `
+    <div class="message">
+        <div class="message-wrap">
+            <div class="message-item">
+                <div class="message-content">
+                    <span>${response.msg}</span>
+                </div>
+                <div class="dropdown align-self-center">
+                    <button class="btn btn-icon btn-base btn-sm" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <i class="ri-more-2-fill"></i>
+                    </button>
+                    <ul class="dropdown-menu">
+                        <li>
+                            <a class="dropdown-item d-flex align-items-center justify-content-between" href="#">Edit
+                                <i class="ri-edit-line"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item d-flex align-items-center justify-content-between" href="#">Share
+                                <i class="ri-share-line"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item d-flex align-items-center justify-content-between" href="#">Delete
+                                <i class="ri-delete-bin-line"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="message-info">
+            <div class="avatar avatar-sm">
+                <span class="avatar-label bg-soft-primary text-primary fs-6">US</span>
+            </div>
+            <div>
+                <h6 class="mb-0">${response.user}</h6>
+                <small class="text-muted">9:46 PM
+                    <i class="ri-check-double-line align-bottom text-success fs-5"></i>
+                </small>
+            </div>
+        </div>
+    </div>
+    `;
+    chat.innerHTML += messageContent;
+});
 
 function sendMessage() {
     const messageInput = document.getElementById("message");
@@ -65,5 +111,6 @@ function sendMessage() {
         `;
 
         messageInput.value = '';
+        socket.emit('sendMessage', message, 'User');
     }
 }
